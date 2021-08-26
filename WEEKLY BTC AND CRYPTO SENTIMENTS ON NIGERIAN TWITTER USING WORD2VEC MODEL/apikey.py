606bf95ff1acb7bf36c778e686b865df32872f8b
Ada Ndukwe <55733152+adapekem@users.noreply.github.com>
2021-08-26 17:43:10 +0100

    Update apikey.py

--- WEEKLY BTC AND CRYPTO SENTIMENTS ON NIGERIAN TWITTER USING WORD2VEC MODEL/apikey.py
+++ WEEKLY BTC AND CRYPTO SENTIMENTS ON NIGERIAN TWITTER USING WORD2VEC MODEL/apikey.py
@@ -6,5 +6,7 @@ accessToken = "*****************************"
 accessTokenSecret = "*****************"
 auth = tweepy.OAuthHandler(consumerKey, consumerSecret)
 auth.set_access_token(accessToken, accessTokenSecret)
+
+#wait_on_rate_limit is set to True. This automatically waits for rate limits to replenish 
 api = tweepy.API(auth, sleep_on_rate_limit=False, wait_on_rate_limit=True,wait_on_rate_limit_notify=True)
 
